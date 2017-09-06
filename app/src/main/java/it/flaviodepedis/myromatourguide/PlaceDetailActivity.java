ad18b425cd724b93b2ac3c69521205e0ea8b7886
fdepedis <flavio.depedis@gmail.com>
2017-09-06 15:35:02 +0200

    update: - AndroidManifest - insert logic for site URL in activity detail

--- app/src/main/java/it/flaviodepedis/myromatourguide/PlaceDetailActivity.java
+++ app/src/main/java/it/flaviodepedis/myromatourguide/PlaceDetailActivity.java
@@ -2,7 +2,6 @@ package it.flaviodepedis.myromatourguide;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
